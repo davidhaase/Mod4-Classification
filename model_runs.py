df79fb07417193bb7ac0b2ffa0a6d4e4721621f9
Noah Sragow <noah@Noahs-MacBook-Air.local>
2019-06-19 14:39:48 -0400

    initial documentation

--- model_runs.py
+++ model_runs.py
@@ -1,4 +1,7 @@
 class RunDetail():
+    '''
+    Description of the factors kept constant in each attempt.
+    '''
     def __init__(self):
         self.metrics = []
         self.split_strategy = None
@@ -6,17 +9,27 @@ class RunDetail():
         self.targettype = None
 
 class ModelRun():
+    '''
+    Describes experiments and their results considering a constant.
+    Manages the expiriment.
+    '''
     def __init__(self):
         self.attempts = []
         self.details = None # RunDetail
 
 class Attempt():
+    '''
+    Contains the ClassifierModel (independent variable) and RunDetails (control/constant)
+    '''
     def __init__(self):
         self.model = None #ClassifierModel
         self.details = None
 
 class ClassifierModel():
-    def __init__(self):
+    '''
+    Represents decisions from original data all the way until results.
+    '''
+    def __init__(self,df,process):
         self.sklearn_model = None
         self.data_processing = None #Normalizer,Scaling, how to handle columns
         self.?
