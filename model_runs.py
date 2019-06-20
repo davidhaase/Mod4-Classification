890d6338bcd24295dad4e95687595bcf3a1bc9bb
David Haase <davidhenryhaase@aol.com>
2019-06-20 18:25:31 -0400

    Updated with working Max Score and Line Plot

--- model_runs.py
+++ model_runs.py
@@ -1,3 +1,25 @@
+import pandas as pd
+import numpy as np
+
+
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.feature_selection import SelectKBest, f_regression, mutual_info_regression
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', 300)
+
+import warnings
+warnings.filterwarnings('ignore')
+
 class ModelRun():
     '''
     Describes experiments and their results considering a constant.
@@ -7,17 +29,31 @@ class ModelRun():
         self.df = df
         self.history = []
 
+
     def get_results(self, score_type):
-        import matplotlib.pyplot as plt
+        max_value = 0.00
+        max_trial = None
         scores = []
         trial_name = []
         trial_range = len(self.history)
-
-        for trial in self.history:
-            key, result = trial
-            if score_type in result:
-                scores.append(result[score_type])
+        for key, result_agg in self.history:
+            if score_type in result_agg:
+                score = result_agg[score_type]
+                scores.append(score)
                 trial_name.append(key)
+                if score > max_value:
+                    max_value = score
+                    max_trial = key
+
+        df_scores = pd.DataFrame(scores)
+
+        df_scores.plot.line(figsize=(16,8))
+
+        print('***\nWINNER for {}: {}'.format(score_type, str(max_value)))
+        for k in key:
+            print ('{}: {}'.format(k, key[k]))
+        return scores
+
 
         # plt.figure(figsize=(12, 6))
         # plt.plot(trial_range, scores, color='red', linestyle='dashed', marker='o',
@@ -27,9 +63,66 @@ class ModelRun():
         # plt.ylabel(score_type)
         # plt.show()
 
-        for i in  trial_name:
-            print('{}: {}\n'.format(str(trial_num), trial))
+    def explore_data(self):
+        self.df.shape
 
+        sns.set(style="white")
+
+        # Compute the correlation matrix
+        corr = self.df.corr()
+
+        # Generate a mask for the upper triangle
+        mask = np.zeros_like(corr, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+
+        # Set up the matplotlib figure
+        f, ax = plt.subplots(figsize=(11, 9))
+
+        # Generate a custom diverging colormap
+        cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+        # Draw the heatmap with the mask and correct aspect ratio
+        sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+                    square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+        plt.savefig('images/correlation_heatmap.png')
+
+    def plot_pairs(self, cols=[]):
+        if cols == []:
+            cols = self.df.columns
+
+        sns.set(style="white")
+        sns.pairplot(self.df, vars=cols)
+        plt.savefig('images/pairplots.png')
+
+    def missing_data(self):
+        #missing data
+        total = self.df.isnull().sum().sort_values(ascending=False)
+        percent = (self.df.isnull().sum()/self.df.isnull().count()).sort_values(ascending=False)
+        missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+        return missing_data.head(20)
+
+    def get_drop_list_corrs(self, threshold=0.95):
+
+        # Create correlation matrix
+        # Select upper triangle of correlation matrix
+        corr_matrix = self.df.corr().abs()
+        upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+        return [column for column in upper.columns if any(upper[column] > threshold)]
+
+    # def get_drop_list_f_test(self, k=10):
+    #     selector = SelectKBest(f_regression, k)
+    #     selector.fit(self.X_train, self.y_train)
+    #     return self.X_train.columns[~selector.get_support()]
+
+    def show_target_balance(self,target):
+        sns.set(style="white")
+        plt.figure(figsize = (10,5))
+        sns.countplot(self.df[target], alpha =.80, palette= ['grey','lightgreen'])
+        plt.title('Non-Defaults vs Defaults')
+        plt.ylabel('# Defaults')
+        plt.show()
+        plt.savefig('images/balance.png')
 
 
     def run_model(self, rd):
